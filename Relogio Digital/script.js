1231b60423b85144fa624dcf42da14779f86fcb2
Matheus Felix Barbosa <145235787+MatheusBarb19@users.noreply.github.com>
2024-02-22 21:10:24 -0300

    Relogio Digital
    Projeto Finalizado

--- Relogio Digital/script.js
+++ Relogio Digital/script.js
@@ -2,15 +2,28 @@ const horas = document.getElementById("horas");
 const minutos = document.getElementById("minutos");
 const segundos = document.getElementById("segundos");
 
-const relogio = setInterval(function time(){
-    let dateToday = new Date(); //Atribui a data de Hj a varivel 
+const relogio = setInterval(function time() {
 
-    let hr = dateToday.getHour(); //Pegar a Hora da variavel
+    let dateToday = new Date(); //Atribui a data de Hj a varivel 
+    let hr = dateToday.getHours(); //Pegar a Hora da variavel
     let min = dateToday.getMinutes(); //Pegar os minutos da variavel
     let s = dateToday.getSeconds(); //Pegar os segundos da variavel
 
-    horas.textContent = hr; // muta o texto no html
-    minutos.textContent = minutos;
-    segundos.textCont = segundos;
+
+    if (hr <10){
+        hr = "0" + hr;} //adciona o numero 0 se for menor que 10
+
+    if (min <10){
+        min = "0" + min;} //adciona o numero 0 se for menor que 10
+
+    if (s <10){
+        s = "0" + s;} //adciona o numero 0 se for menor que 10 
+
+    horas.textContent = hr; // muda o texto no html
+    minutos.textContent = min;
+    segundos.textContent = s;
 })
+
+
+
 
